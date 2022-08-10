6e13842ae0f0c8bbeacc3bc0c0508062c63c2819
Thomas <malifarge.thomas9@gmail.com>
2022-08-10 12:57:36 +0200

    clean success

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,15 +1,26 @@
 import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 const Success = () =>{
-
+    const [student,setStudent] = useState("")
     const Studentname = useParams()
+
+    const getStudents = async () => {
+        const request = await fetch(`http://localhost:5000/Students/${Studentname.name}`)
+        const response = await request.json()
+        setStudent(response)
+      }
+
+    useEffect(()=>{
+        getStudents()
+    },[])
 
     return ( 
 
         <section>
             <h1>Success</h1>
 
-            <p>Congratulations, you have added {Studentname.name} to the list of students</p>
+            <p>Congratulations, you have added {student.name} to the list of students</p>
 
         </section>
     )
